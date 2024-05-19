a2137dbb39afe1f9b7112e45d048158fa2842716
박지원 <jackgg12322@gmail.com>
2024-05-20 01:37:34 +0900

    feat : Root component Suspense 활용

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,15 +2,20 @@ import { Outlet } from 'react-router-dom';
 import SearchHeader from '../components/header/SearchHeader';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
+import { Suspense } from 'react';
 
 export default function Root() {
   const queryClient = new QueryClient();
   return (
-    <RecoilRoot>
-      <SearchHeader />
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchHeader />
+          <QueryClientProvider client={queryClient}>
+            <Outlet />
+          </QueryClientProvider>
+        </Suspense>
+      </RecoilRoot>
+    </>
   );
 }
